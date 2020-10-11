0bca3e4a953f9b8934f79813ea120d38a8fa47a6
RobLabs.com <roblabs@users.noreply.github.com>
2020-10-11 15:50:12 -0700

    One button build - update `git patch` script

--- examples/git_patch.sh
+++ examples/git_patch.sh
@@ -9,21 +9,21 @@ set -v
 cd ${XCS_PRIMARY_REPO_DIR}
 pwd
 
-# MARK: - change branch if XCS does not give you the option
 /usr/bin/git apply \
   ~/Downloads/patch-teamID.diff
-
-# Verify what is changed
-/usr/bin/git diff
 
 set +v
 
 echo XCS_PRIMARY_REPO_DIR = $XCS_PRIMARY_REPO_DIR
 
-# Git metadata
+# MARK: - Git metadata
 git remote --verbose
 git branch
 gitBranch=$(/usr/bin/git -C ${XCS_PRIMARY_REPO_DIR} rev-parse --abbrev-ref HEAD)
 echo branch = $gitBranch
 gitSHA=$(/usr/bin/git -C ${XCS_PRIMARY_REPO_DIR} rev-parse HEAD)
 echo SHA = $gitSHA
+
+# MARK: - Verify what is changed
+/usr/bin/git status
+/usr/bin/git diff
