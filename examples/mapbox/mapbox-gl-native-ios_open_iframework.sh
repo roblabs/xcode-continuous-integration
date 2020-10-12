c53cb4c4c8365a7757ae92c99face3dbc7618df8
RobLabs.com <roblabs@users.noreply.github.com>
2020-10-12 09:19:42 -0700

    One button builds - Log version metadata and save off framework, docs & license

--- examples/mapbox/mapbox-gl-native-ios_open_iframework.sh
+++ examples/mapbox/mapbox-gl-native-ios_open_iframework.sh
@@ -18,17 +18,36 @@ cd ${XCS_PRIMARY_REPO_DIR}
 pwd
 
 open build/ios/pkg
+
 echo ${XCS_PRIMARY_REPO_DIR}/build/ios/pkg
+
 cat build/ios/pkg/version.txt
+
+echo open ${XCS_PRIMARY_REPO_DIR}/build/ios/pkg/documentation/index.html
+
+cd ${XCS_PRIMARY_REPO_DIR}/build/ios/pkg
 
 # Version info from Mapbox.framework
 # plutil -- property list utility
   # plutil -help
   # man plutil
 
-plutil -p build/ios/pkg/dynamic/Mapbox.framework/Info.plist | grep CFBundleIdentifier
-plutil -p build/ios/pkg/dynamic/Mapbox.framework/Info.plist | grep CFBundleShortVersionString
-plutil -p build/ios/pkg/dynamic/Mapbox.framework/Info.plist | grep CFBundleVersion
-plutil -p build/ios/pkg/dynamic/Mapbox.framework/Info.plist | grep MGLSemanticVersionString
-plutil -p build/ios/pkg/dynamic/Mapbox.framework/Info.plist | grep MGLCommitHash
-plutil -p build/ios/pkg/dynamic/Mapbox.framework/Info.plist
+plutil -convert json dynamic/Mapbox.framework/Info.plist -o Info.json -r
+# `json` depends on https://www.npmjs.com/package/json
+#    npm install -g json
+cat Info.json | json CFBundleIdentifier
+cat Info.json | json CFBundleShortVersionString
+cat Info.json | json CFBundleVersion
+cat Info.json | json MGLSemanticVersionString
+cat Info.json | json MGLCommitHash
+
+# make script to move Mapbox.framework, docs, and license
+SHORT_VERSION="$(cat Info.json | json CFBundleShortVersionString)"
+echo "mv pkg/ ~/Downloads/${SHORT_VERSION}" > ../mv-pkg.sh
+
+plutil -p dynamic/Mapbox.framework/Info.plist | grep CFBundleIdentifier
+plutil -p dynamic/Mapbox.framework/Info.plist | grep CFBundleShortVersionString
+plutil -p dynamic/Mapbox.framework/Info.plist | grep CFBundleVersion
+plutil -p dynamic/Mapbox.framework/Info.plist | grep MGLSemanticVersionString
+plutil -p dynamic/Mapbox.framework/Info.plist | grep MGLCommitHash
+plutil -p dynamic/Mapbox.framework/Info.plist
